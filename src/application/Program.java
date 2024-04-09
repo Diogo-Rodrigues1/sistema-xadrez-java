fbf9c96a20363b53366db29d834f5e2c824b8fa7
Diogo-Rodrigues1 <diogorodrigues31415@gmail.com>
2024-04-09 16:53:11 -0300

    Special move - En passant

--- src/application/Program.java
+++ src/application/Program.java
@@ -15,7 +15,7 @@ public class Program {
 		ChessMatch chessMatch = new ChessMatch();
 		List<ChessPiece> captured = new ArrayList<>();
 		
-		while (true) {
+		while (!chessMatch.getCheckMate()) {
 			try {
 				UI.clearScreen();
 				UI.printMatch(chessMatch, captured);
@@ -30,21 +30,22 @@ public class Program {
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
 
-				
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
 				
 				if(capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
 			}
-			catch (ChessException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
-			}
-			catch (InputMismatchException e) {
-				System.out.println(e.getMessage());
-				sc.nextLine();
+				catch (ChessException e) {
+					System.out.println(e.getMessage());
+					sc.nextLine();
+				}
+				catch (InputMismatchException e) {
+					System.out.println(e.getMessage());
+					sc.nextLine();
 			}
 		}
+		UI.clearScreen();
+		UI.printMatch(chessMatch, captured);
 	}
 }
