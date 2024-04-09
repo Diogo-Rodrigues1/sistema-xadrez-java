7060bc0a3497ccd90005385a53334fe517d7afab
Diogo-Rodrigues1 <diogorodrigues31415@gmail.com>
2024-04-09 15:10:57 -0300

    Special move - Castling

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,6 +86,24 @@ public class ChessMatch {
 			capturedPieces.add(capturedPiece);
 		}
 		
+		// ROQUE1
+		if(p instanceof King && target.getColumn() == source.getColumn() + 2) {
+			Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+			Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+			ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+			board.placePiece(rook, targetT);
+			rook.increaseMoveCount();
+		}
+		
+		// ROQUE2
+				if(p instanceof King && target.getColumn() == source.getColumn() - 2) {
+					Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+					Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+					ChessPiece rook = (ChessPiece)board.removePiece(sourceT);
+					board.placePiece(rook, targetT);
+					rook.increaseMoveCount();
+				}
+		
 		return capturedPiece;
 	}
 	
@@ -99,6 +117,25 @@ public class ChessMatch {
 			capturedPieces.remove(capturedPiece);
 			capturedPieces.add(capturedPiece);
 		}
+		
+		// ROQUE1
+				if(p instanceof King && target.getColumn() == source.getColumn() + 2) {
+					Position sourceT = new Position(source.getRow(), source.getColumn() + 3);
+					Position targetT = new Position(source.getRow(), source.getColumn() + 1);
+					ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+					board.placePiece(rook, sourceT);
+					rook.decreaseMoveCount();
+				}
+				
+				// ROQUE2
+						if(p instanceof King && target.getColumn() == source.getColumn() - 2) {
+							Position sourceT = new Position(source.getRow(), source.getColumn() - 4);
+							Position targetT = new Position(source.getRow(), source.getColumn() - 1);
+							ChessPiece rook = (ChessPiece)board.removePiece(targetT);
+							board.placePiece(rook, sourceT);
+							rook.decreaseMoveCount();
+						}
+		
 	}
 	
 	private void validateSourcePosition(Position position) {
@@ -162,7 +199,7 @@ public class ChessMatch {
 		placeNewPiece('g', 1, new Knight(board, Color.WHITE));
 		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
 		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
-        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE, this));
         placeNewPiece('h', 1, new Rook(board, Color.WHITE));
         placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
         placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -177,7 +214,7 @@ public class ChessMatch {
         placeNewPiece('d', 8, new Queen(board, Color.BLACK));
         placeNewPiece('b', 8, new Knight(board, Color.BLACK));
         placeNewPiece('g', 8, new Knight(board, Color.BLACK));
-        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK, this));
         placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
         placeNewPiece('h', 8, new Rook(board, Color.BLACK));
